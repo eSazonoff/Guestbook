65a9f3b226b647de21a4c513f6b124fa8f03f60e
SALONBLOOM54 <klok-nikita@mail.ru>
2021-03-29 01:22:50 +0300

    Update index.php
    Добавление htmlspecialchars.
    Абсолютный путь до папки через __DIR__.

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 </head>
 <body>
 <?php
-	$folderName = 'messages'; // папка, куда будут сохраняться сообщения
+	//путь до папки абсолютный (константа __DIR__)
+	$folderName = __DIR__.'/messages';
 
 //Если папки с таким именем не существует, то создаём новую
 	if (!is_dir($folderName)) mkdir($folderName); // is_dir — определяет, является ли имя файла каталогом, mkdir — создаёт каталог
@@ -14,21 +15,22 @@
 //Отображение всех существующих сообщений в папке messages
 	$count = count($messages); //count — подсчитывает количество элементов массива 
 	for ($i = 2; $i < $count; $i++) { // исключаем специальные управляющие символы "." (точка) и ".." (две точки)
-        $currentFileName = $messages[$i];
-        $arrTextMessage = file("messages/$currentFileName"); //получает содержимое файла в виде массива
+		$currentFileName = $messages[$i];
+		$arrTextMessage = file("messages/$currentFileName"); //получает содержимое файла в виде массива
 
-        $name = $arrTextMessage[0]; // 1-я строка файла - имя автора сообщения
-        $mail = $arrTextMessage[1]; // 2-я строка - email автора сообщения
-        echo htmlspecialchars($name).'<br/>'; //htmlspecialchars — преобразует специальные символы в HTML-сущности
-        echo "<a href='mailto:$mail'>$mail</a>".'<br/>';
+		$name = htmlspecialchars($arrTextMessage[0]); //htmlspecialchars — преобразует специальные символы в HTML-сущности
+		$mail = htmlspecialchars($arrTextMessage[1]);
+		echo $name .'<br/>'; 
+		echo "<a href='mailto:$mail'>$mail</a>".'<br/>';
 
-        for ($j = 2; $j < count($arrTextMessage); $j++) { //3-я и последующие строки - текст сообщения
-        	echo htmlspecialchars($arrTextMessage[$j]); //htmlentities
-        	echo '<br/>';
-        }
+		for ($j = 2; $j < count($arrTextMessage); $j++) { //3-я и последующие строки - текст сообщения
+			echo htmlspecialchars($arrTextMessage[$j]);
+			echo '<br/>';
+		}
+		echo '<hr/>';
     }
 
-//Создание формы ввода: имя, мыло, текст
+//Формы ввода: имя, мыло, текст
     echo '<form action="index.php" method="post">
 	 <p>Your Name: <input type="text" name="name" /></p>
 	 <p>Your Email: <input type="email" name="email" /></p>
@@ -38,7 +40,8 @@
 	$name1 = $_POST['name']; //принимает значение name через POST
 	$email1 = $_POST['email'];
 	$message1 = $_POST['message'];
-//Отправка нового сообщения
+
+//Запись нового сообщения
 	if ($name1 && $email1 && $message1 && filter_var($email1, FILTER_VALIDATE_EMAIL)) { // возвращает отфильтрованные данные или false, если фильтрация завершилась неудачей.
 		$time = date('d.m.Y_h.i.s'); //вывод системной даты/времени
 		$newFile = "messages/$time.txt";
@@ -46,8 +49,8 @@
 		file_put_contents($newFile, $textNewFile); //файл будет создан, если его нет; запись данных в указанный файл
 
 		echo htmlspecialchars($name1).'<br/>';
-        echo "<a href='mailto:$email1'>$email1</a>".'<br/>';
-        echo htmlspecialchars($message1) .'<br/>';
+		echo "<a href='mailto:$email1'>$email1</a>".'<br/>';
+		echo htmlspecialchars($message1) .'<br/>';
 	} else {
 		echo 'Error while adding your message!';
 	}
