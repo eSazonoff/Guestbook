84e6b2ebe7e05291d9b948fff56df323175becdf
SALONBLOOM54 <klok-nikita@mail.ru>
2021-03-29 02:40:58 +0300

    Принятие и проверка формы
    Проверь получение данных из формы изпользуя isset
    
    Добавлена проверка данных из формы с помощью issert.
    Решена проблема с дублированием сообщения.
    
    Проверка полученых данных из формы с помощью функции issert

--- index.php
+++ index.php
@@ -5,18 +5,45 @@
 </head>
 <body>
 <?php
-	//путь до папки абсолютный (константа __DIR__)
-	$folderName = __DIR__.'/messages';
+	$folderName = __DIR__.'/messages'; //абсолютный путь к папке
 
 //Если папки с таким именем не существует, то создаём новую
 	if (!is_dir($folderName)) mkdir($folderName); // is_dir — определяет, является ли имя файла каталогом, mkdir — создаёт каталог
 	$messages = scandir($folderName); //scandir передает имена файлов из папки
 
+	//ПРИНЯТИЕ ФОРМЫ И СОХРАНЕНИЕ СООБЩЕНИЙ В ПАПКЕ
+//данные формы принимаю до того как отобразятся сообщения. То есть получая новое сообщение, сперва сохраняем его, а потом выводим все сообщения включая новые.
+
+$error_info = '';//сообщение об ошибке
+
+//Проверка формы на submit
+if(isset($_POST['submit']))
+{
+	$name = isset($_POST['name']) ? trim($_POST['name']) : '';
+	$email = isset($_POST['email']) ? trim($_POST['email']) : '';
+	$message = isset($_POST['message']) ? trim($_POST['message']) : '';
+
+	if(strlen($name) > 0 && strlen($email) > 0 && strlen($message) > 0 && filter_var($email, FILTER_VALIDATE_EMAIL) !== false) //фильтр-проверка на правильность и наличие вводимых пользователем данных
+	{
+		$time = date('d.m.Y_h.i.s'); //вывод системной даты/времени
+		
+		$save_file = $messages.'/'.$time;
+		$save_data = "{$name}\n{$email}\n{$message}";
+		file_put_contents($save_file, $save_data);
+	}
+	else
+	{
+		$error_info = "<p>Error while adding your message.</p>";
+	}
+}
+
+echo $error_info;
+
 //Отображение всех существующих сообщений в папке messages
 	$count = count($messages); //count — подсчитывает количество элементов массива 
 	for ($i = 2; $i < $count; $i++) { // исключаем специальные управляющие символы "." (точка) и ".." (две точки)
 		$currentFileName = $messages[$i];
-		$arrTextMessage = file("messages/$currentFileName"); //получает содержимое файла в виде массива
+		$arrTextMessage = file("{$messages}/".$currentFileName); //получает содержимое файла в виде массива
 
 		$name = htmlspecialchars($arrTextMessage[0]); //htmlspecialchars — преобразует специальные символы в HTML-сущности
 		$mail = htmlspecialchars($arrTextMessage[1]);
@@ -35,25 +62,9 @@
 	 <p>Your Name: <input type="text" name="name" /></p>
 	 <p>Your Email: <input type="email" name="email" /></p>
 	 <textarea name="message"></textarea>
-	 <p><input type="submit" /></p>
+	 <p><input name="submit" type="submit" /></p>
 	</form>';
-	$name1 = $_POST['name']; //принимает значение name через POST
-	$email1 = $_POST['email'];
-	$message1 = $_POST['message'];
 
-//Запись нового сообщения
-	if ($name1 && $email1 && $message1 && filter_var($email1, FILTER_VALIDATE_EMAIL)) { // возвращает отфильтрованные данные или false, если фильтрация завершилась неудачей.
-		$time = date('d.m.Y_h.i.s'); //вывод системной даты/времени
-		$newFile = "messages/$time.txt";
-		$textNewFile = "$name1\n$email1\n$message1";
-		file_put_contents($newFile, $textNewFile); //файл будет создан, если его нет; запись данных в указанный файл
-
-		echo htmlspecialchars($name1).'<br/>';
-		echo "<a href='mailto:$email1'>$email1</a>".'<br/>';
-		echo htmlspecialchars($message1) .'<br/>';
-	} else {
-		echo 'Error while adding your message!';
-	}
 //НЕИСПРАВНОСТЬ: При обновлении страницы новое сообщение дублируется
 ?>
 </body>
