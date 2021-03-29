e24c5c26e87868312e809fef1e5eb9088e13a7c6
SALONBLOOM54 <klok-nikita@mail.ru>
2021-03-29 05:24:12 +0300

    Refactoring index.php :gem:
    Clean code
    
    Группировка кода по задачам.
    Измененные названия переменных.
    Отредактированные комментарии.

--- index.php
+++ index.php
@@ -5,67 +5,60 @@
 </head>
 <body>
 <?php
-	$folderName = __DIR__.'/messages'; //абсолютный путь к папке
+	$folderName = __DIR__.'/messages';
 
-//Если папки с таким именем не существует, то создаём новую
-	if (!is_dir($folderName)) mkdir($folderName); // is_dir — определяет, является ли имя файла каталогом, mkdir — создаёт каталог
-	$messages = scandir($folderName); //scandir передает имена файлов из папки
+	if (!is_dir($folderName)) {
+		mkdir($folderName);
+	}
 
-	//ПРИНЯТИЕ ФОРМЫ И СОХРАНЕНИЕ СООБЩЕНИЙ В ПАПКЕ
-//данные формы принимаю до того как отобразятся сообщения. То есть получая новое сообщение, сперва сохраняем его, а потом выводим все сообщения включая новые.
+	$messages = scandir($folderName);
 
-$error_info = '';//сообщение об ошибке
+//Данные формы принимаютя до отображения сообщения. Значит получая новое сообщение, сперва сохраняем его, а потом выводим все сообщения из папки, включая новые.
 
-//Проверка формы на submit
-if(isset($_POST['submit']))
-{
-	$name = isset($_POST['name']) ? trim($_POST['name']) : '';
+//Проверка данных получаемых формой
+if (isset($_POST['submit'])) {
+	$author = isset($_POST['author']) ? trim($_POST['author']) : ''; //удаляем пробелы из строки, иначе пустая строка
 	$email = isset($_POST['email']) ? trim($_POST['email']) : '';
 	$message = isset($_POST['message']) ? trim($_POST['message']) : '';
 
-	if(strlen($name) > 0 && strlen($email) > 0 && strlen($message) > 0 && filter_var($email, FILTER_VALIDATE_EMAIL) !== false) //фильтр-проверка на правильность и наличие вводимых пользователем данных
-	{
-		$time = date('d.m.Y_h.i.s'); //вывод системной даты/времени
-		
-		$save_file = $messages.'/'.$time;
-		$save_data = "{$name}\n{$email}\n{$message}";
+	if (strlen($author) > 0 && strlen($email) > 0 && strlen($message) > 0 && filter_var($email, FILTER_VALIDATE_EMAIL) !== false) {
+		$time = date('d.m.Y_h.i.s');
+		$save_file = $folderName . '/' . $time;
+		$save_data = "{$author}\n{$email}\n{$message}";
 		file_put_contents($save_file, $save_data);
-	}
-	else
-	{
-		$error_info = "<p>Error while adding your message.</p>";
+	} else {
+		echo 'Error while adding your message.' . '<br/>';
 	}
 }
 
-echo $error_info;
-
 //Отображение всех существующих сообщений в папке messages
-	$count = count($messages); //count — подсчитывает количество элементов массива 
-	for ($i = 2; $i < $count; $i++) { // исключаем специальные управляющие символы "." (точка) и ".." (две точки)
-		$currentFileName = $messages[$i];
-		$arrTextMessage = file("{$messages}/".$currentFileName); //получает содержимое файла в виде массива
+$messages = scandir($folderName);
+$count = count($messages);
 
-		$name = htmlspecialchars($arrTextMessage[0]); //htmlspecialchars — преобразует специальные символы в HTML-сущности
-		$mail = htmlspecialchars($arrTextMessage[1]);
-		echo $name .'<br/>'; 
-		echo "<a href='mailto:$mail'>$mail</a>".'<br/>';
+for ($i = 2; $i < $count; $i++) { //исключаем управляющие символы каталога, занимающие первые две ячейки
+	$currentFileName = $messages[$i];
+	$arrTextMessage = file("{$folderName}/" . $currentFileName);
 
-		for ($j = 2; $j < count($arrTextMessage); $j++) { //3-я и последующие строки - текст сообщения
-			echo htmlspecialchars($arrTextMessage[$j]);
-			echo '<br/>';
-		}
-		echo '<hr/>';
-    }
+	$author = htmlspecialchars($arrTextMessage[0]);
+	$mail = htmlspecialchars($arrTextMessage[1]);
 
-//Формы ввода: имя, мыло, текст
-    echo '<form action="index.php" method="post">
-	 <p>Your Name: <input type="text" name="name" /></p>
-	 <p>Your Email: <input type="email" name="email" /></p>
-	 <textarea name="message"></textarea>
-	 <p><input name="submit" type="submit" /></p>
+	echo $author . '<br/>';
+	echo "<a href='mailto:$mail'>mailto:$mail</a>" . '<br/>';
+
+	for ($j = 2; $j < count($arrTextMessage); $j++) {
+		echo htmlspecialchars($arrTextMessage[$j]) . '<br/>';
+	}
+
+	echo '<hr/>';
+}
+
+//Форма ввода данных: имя, почта, сообщение
+echo '<form action="index.php" method="post">
+	<p>Your Name: <input type="text" name="author" /></p>
+	<p>Your Email: <input type="email" name="email" /></p>
+	<textarea name="message"></textarea>
+	<p><input name="submit" type="submit" /></p>
 	</form>';
-
-//НЕИСПРАВНОСТЬ: При обновлении страницы новое сообщение дублируется
 ?>
 </body>
 </html>
